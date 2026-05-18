64cc5d7f2e95154125cad3148c40ed91fc584fd8
axelescobar-art <axelescobar@liceovvh.cl>
2026-05-18 14:23:40 -0400

    ok

---
+++ STACK_JS/01_introduccion_Js/static/js/script.js
@@ -0,0 +1,14 @@
+console.log(conexion)
+
+function cambiarTexto() {
+    let titulo = "Hola, Bienvenido";
+    if (document.getElementById("titulo").innerText = "titulo") {
+        document.getElementById("titulo").innerText = "Has cambiado el texto con JS"
+        document.getElementById("titulo").style.color= "red";
+    }else
+    document.getElementById("titulo").innerText = "titulo";
+    document.getElementById("titulo").style.color= "black";{
+    }
+
+}
+
